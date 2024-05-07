48e078714d8f5bb5a629d2d299a72d4ca530f4e4
HoneyPradhan <130152598+HoneyPradhan@users.noreply.github.com>
2024-05-07 10:52:37 +0530

    Update jobsTypeRoute.js

--- routes/jobsTypeRoute.js
+++ routes/jobsTypeRoute.js
@@ -6,14 +6,14 @@ const { isAuthenticated,isAdmin} = require('../middleware/auth')
 //job type routes
 
 // /api/type/create
-router.post('/type/create',isAuthenticated,createJobType)
+router.post('/type/create',createJobType)
 
 // /api/type/jobs
 router.get('/type/jobs',allJobsType)
 
  //   /api/type/update/:type_id
-router.put('/type/update/:type_id',isAuthenticated,updateJobsType)
+router.put('/type/update/:type_id',updateJobsType)
 // /api/type/delete/:type_id
 
-router.delete('/type/delete/:type_id',isAuthenticated,deleteJobsType)
+router.delete('/type/delete/:type_id',deleteJobsType)
 module.exports= router
